2b40b49760b3d6d1bf1a74f944a425d6b79ccbd2
richard <richard@maqe.com>
2022-10-23 17:20:51 +0700

    configure ios image notification

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by Richard on 23/10/2565 BE.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
