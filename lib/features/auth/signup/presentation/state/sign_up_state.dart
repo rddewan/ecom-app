efff2a4a709acd08b86f36f88a0261eefef72663
richard <richard@maqe.com>
2022-12-05 16:01:56 +0700

    added SignUpState

---
+++ lib/features/auth/signup/presentation/state/sign_up_state.dart
@@ -0,0 +1,23 @@
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'sign_up_state.freezed.dart';
+
+@freezed
+class SignUpState with _$SignUpState {
+
+  const factory SignUpState({
+    @Default(AsyncData(false))
+    final AsyncValue<bool> isSignUp,
+    @Default(false)
+    final bool isLoading,
+    @Default(true)
+    final bool isObscure,
+    @Default(false)
+    final bool isTermsAndCondition,
+    @Default({})
+    final Map<String,dynamic> formData,
+
+  }) = _SignUpState;
+}
