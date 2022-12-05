3f28f5e7366de1701bdd3746f6873559a949baa8
richard <richard@maqe.com>
2022-12-05 17:05:42 +0700

    reuasble widget scaffold

---
+++ lib/common/widget/app_scaffold.dart
@@ -0,0 +1,35 @@
+
+import 'package:flutter/material.dart';
+
+class AppScaffold extends StatelessWidget {
+  final Widget title;
+  final Widget widget;
+  final bool centerTitle;
+  final ScrollController? controller;
+  
+  const AppScaffold({
+    Key? key,
+    required this.title,
+    required this.widget,
+    this.centerTitle = true,
+    this.controller,
+    
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: title,
+        centerTitle: centerTitle,
+        elevation: 0,
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          controller: controller,
+          child: widget,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/common/widget/app_scaffold_sliver.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class AppScaffoldSliver extends StatelessWidget {
+  final Widget title;
+  final List<Widget> slivers;
+  final bool centerTitle;
+  final ScrollController? controller;
+  
+  const AppScaffoldSliver({
+    Key? key,
+    required this.title,
+    required this.slivers,
+    this.centerTitle = true,
+    this.controller,
+    
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: title,
+        centerTitle: centerTitle,
+        elevation: 0,
+      ),
+      body: SafeArea(
+        child: CustomScrollView(
+          controller: controller,
+          slivers: slivers,
+        ),
+      ),
+    );
+  }
+}
