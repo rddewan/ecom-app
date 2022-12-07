4e85abd3932988230a76805ee6a0622580940fb9
richard <richard@maqe.com>
2022-12-07 08:48:28 +0700

    added login state

---
+++ lib/features/auth/login/presentation/state/login_state.dart
@@ -0,0 +1,23 @@
+
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'login_state.freezed.dart';
+
+@freezed
+class LoginState with _$LoginState {
+  const factory LoginState({
+    @Default(AsyncValue.data(false))
+    final AsyncValue<bool> isLoggedIn,
+    @Default(false)
+    final bool isLoading,
+    @Default(true)
+    final bool isObscure,
+    @Default({})
+    final Map<String, dynamic> formData,
+
+  }) = _LoginState;
+
+
+}
