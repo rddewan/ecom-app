bbb22f80dc45cdf25bfa5fdef21b64f7cf953100
richard <richard@maqe.com>
2022-12-08 10:26:35 +0700

    added product state

---
+++ lib/features/product/presentation/state/product_state.dart
@@ -0,0 +1,25 @@
+
+import 'package:ecom_app/features/product/domain/models/product.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'product_state.freezed.dart';
+
+@freezed
+class ProductState with _$ProductState {
+
+  const factory ProductState({
+    @Default(true)
+    bool isLoading,
+    @Default(false)
+    bool isFetching,
+    @Default(0)
+    int currentPage,
+    @Default(0)
+    int totalPage,
+    @Default(0)
+    int total,
+    @Default([])
+    List<Product> products,
+
+  }) = _ProductState;
+}
